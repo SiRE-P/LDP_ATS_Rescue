429c129938016c3cb72b1b769f4f4a10b9ec9c7a
Alice Assmar <aliceassmar92@gmail.com>
2025-11-19 22:03:13 -0500

    First commit AA's branch. Initial codes and output.

---
+++ TRAWL_BIOSAMPLE/01_code/Read_Trawl_data_from_SAS_datasets.R
@@ -0,0 +1,249 @@
+###############################################################################
+##############            TRAWL data code 1977-1999         ###################
+##############    Converting and cleaning SAS trawl files   ###################
+############## Authors: Alice Assmar (McGill Uni.), David   ###################
+############## Hunt (McGill Uni.),  Yuliya Shtymburski      ###################
+############## (U. Regina), Howard Stiff (DFO Nanaimo)      ###################
+##############        Athena Ogden (DFO Nanaimo)            ###################
+###############################################################################
+
+# getwd()
+# setwd("/LDP_ATS_Rescue")
+
+# Install necessary packages if they are not yet installed
+packages <- c("beepr", "dplyr", "lubridate","progress",
+              "purrr","stringr", "tibble", "tictoc", "tidyverse", "tools", "Rcpp", "haven")
+install.packages(setdiff(packages, row.names(installed.packages())))
+
+# Load necessary packages
+{
+  library(beepr)
+  library(dplyr)
+  library(lubridate)
+  library(purrr)
+  library(stringr)
+  library(tibble)       # install.packages("tictoc")
+  library(tictoc)       # get elapsed time 
+  library(tidyverse)
+  library(tools)
+  library(Rcpp) 
+  library(haven) # read SAS files
+}
+
+################################  Step 1  #####################################
+########################## Organize the directory #############################
+
+# Uncomment this section if you'd like to rename the folders
+# Renaming existing directory if it is not standardized.
+# Be careful to not delete the current script
+#if (!dir.exists("./TRAWL_BIOSAMPLE/01_code")) {
+#  if(dir.exists("./TRAWL_BIOSAMPLE/code")) {
+#    renaming_folder <- file.rename("./TRAWL_BIOSAMPLE/code", "./TRAWL_BIOSAMPLE/01_code")
+#    if (renaming_folder){
+#      message("Directory renamed successfully.") 
+#    } else {
+#      warning("Failed to rename directory")
+#    }
+#  } else {
+#    warning("Old directory does not exist.")
+#  }
+#} else {
+#  message("New directory already exists.")
+#}
+#
+## Renaming raw data folder
+#if (!dir.exists("./TRAWL_BIOSAMPLE/00_raw_data")) {
+#  if(dir.exists("./TRAWL_BIOSAMPLE/data")) {
+#    renaming_folder <- file.rename("./TRAWL_BIOSAMPLE/data", "./TRAWL_BIOSAMPLE/00_raw_data")
+#    if (renaming_folder){
+#      message("Directory renamed successfully.") 
+#    } else {
+#      warning("Failed to rename directory")
+#    }
+#  } else {
+#    warning("Old directory does not exist.")
+#  }
+#} else {
+#  message("New directory already exists.")
+#}
+
+# Create the directories to hold output files
+if (!dir.exists("./TRAWL_BIOSAMPLE/02_intermediate_out")) {dir.create("./TRAWL_BIOSAMPLE/02_intermediate_out")
+} else {message("Directory already exists.")} # ensure CSV output directory exists
+if (!dir.exists("./TRAWL_BIOSAMPLE/03_final_output")) {dir.create("./TRAWL_BIOSAMPLE/03_final_output")
+} else {message("Directory already exists.")}  # ensure plot output directory exists
+if (!dir.exists("./TRAWL_BIOSAMPLE/04_figures")) {dir.create("./TRAWL_BIOSAMPLE/04_figures")
+} else {message("Directory already exists.")}  # ensure plot output directory exists
+if (!dir.exists("./TRAWL_BIOSAMPLE/05_ARCHIVE")) {dir.create("./TRAWL_BIOSAMPLE/05_ARCHIVE")
+}  else {message("Directory already exists.")} # ensure archive directory exists for storing date-stamped copy of output
+
+# Regex pattern to automate file processing (loop over all SAS files)
+name_pattern <- "trawl([[:digit:]][[:digit:]])\\.sas7bdat$"
+data_folder <-"./TRAWL_BIOSAMPLE/00_raw_data/02_SAS_Data"
+intermediate_out_folder <- "./TRAWL_BIOSAMPLE/02_intermediate_out"
+
+# Created a function to test the regex. Uncomment it if you'd like to use.
+#see <- function(rx) str_view_all("trawl92.sas7bdat", rx)
+#see("trawl([[:digit:]][[:digit:]])\\.sas7bdat$")
+
+files <- list.files(data_folder, pattern = name_pattern, full.names = TRUE)
+
+# Remove corrupted files, because their metadata have some structural problem
+remove_yy <- c("trawl85.sas7bdat", 
+               "trawl86.sas7bdat",
+               "trawl87.sas7bdat",
+               "trawl94.sas7bdat",
+               "trawl97.sas7bdat", 
+               "trawl98.sas7bdat")
+files <- files[!basename(files) %in% remove_yy]
+
+cat("Found", length(files), "files:\n")
+print(files)
+
+################################  Step 2  #####################################
+################### Read and clean the SAS data files #########################
+
+# Loop over SAS files in the raw folder, combine with metadata and save them in csv
+for (f in files) {
+  cat("\nprocessing file:", f, "\n")
+  
+  trawlyy <- read_sas(f, NULL)
+  metadata_yy <- sub(name_pattern, "\\1", basename(f))
+  metadata_trawl <- read_sas(file.path(data_folder, paste0("trlinf", metadata_yy, ".sas7bdat")))
+  
+  # Combine the metadata matrix with trawl information
+  # Some rows have no observer, so it is better to not use them as a matching column
+  final_df <- full_join(metadata_trawl, trawlyy, by = c("date", "trwlnmbr", "system", "depth", "fspecies"))
+  
+  # Cleaning columns' names
+  rename_map <- c("process_date" = "procdate", 
+                "processor" = "observer.x", 
+                "processor" = "observer",
+                "lake_code" = "system", 
+                "trawl_date" = "date", 
+                "sample_number" = "sample", 
+                "trawl_number" = "trwlnmbr", 
+                "start_time" = "statime", 
+                "end_time" = "endtime",
+                "duration_mi" = "duration", 
+                "depth_m" = "depth", 
+                "species_code" = "fspecies",
+                "aging_technique" = "aged", 
+                "preservative_code" = "prsrv", 
+                "fish_length_mm" = "length",
+                "fish_weight_g" = "weight",
+                "standardized_weight_g" = "stdwght",
+                "scale_book" = "sclbook",
+                "fish_id" = "fishnmbr",
+                "fish_total" = "nmbrfish")
+  
+  # Match with the columns present in the dataframe
+  rename_map <- rename_map[rename_map %in% names(final_df)]
+  # Rename columns
+  final_df <- final_df %>%  
+    rename(!!!rename_map)
+  # Remove duplicated column "observer.y", present in some files
+  if ("observer.y" %in% names(final_df)){
+    final_df$observer.y <- NULL
+    message("Removed column 'observer.y' from table")
+    
+  }
+    
+  ### Creating unique IDs for fishes and Trawls
+  final_df$trawl_unique_ID <- paste(final_df$trawl_date, final_df$lake_code, final_df$trawl_number, sep = "_")
+  final_df$fish_unique_ID <- paste(final_df$trawl_date, final_df$lake_code, final_df$trawl_number, final_df$fish_id, sep = "_")
+  
+  ### save table in csv 
+  write.csv(final_df, paste0(intermediate_out_folder,"/trawl", metadata_yy, "_SAS.csv"), row.names = FALSE)
+
+
+################################  Step 3  #####################################
+################### Create look up tables to organize data ####################
+
+final_df <- read.csv(paste0(intermediate_out_folder,"/trawl", metadata_yy, "_SAS.csv"))
+
+  # check and add new columns to standardize the columns in the spreadsheet
+  new_columns <- c("process_date", "sample_number", 
+                  "scale", "scale_book",
+                  "age", "aging_technique")
+  
+  for (columns in new_columns) {
+    if (!columns %in% names(final_df)) {
+      final_df[[columns]] <- NA 
+  }
+}
+### load your look-up tables
+fish_species_code_lookup_table <- read.csv("./TRAWL_BIOSAMPLE/00_raw_data/04_YS_look_up_tables/fish_species_code_lookup_table.csv")
+
+preservative_code_lookup_table <- read.csv("./TRAWL_BIOSAMPLE/00_raw_data/04_YS_look_up_tables/preservative_code_lookup_table.csv") 
+
+preservative_code_weight_conversion_lookup_table <- read.csv("./TRAWL_BIOSAMPLE/00_raw_data/04_YS_look_up_tables/preservative_code_weight_conv_lookup_table.csv")
+
+lake_name_lookup_table <- read.csv("./TRAWL_BIOSAMPLE/00_raw_data/04_YS_look_up_tables/lake_name_table.csv")
+
+aging_technique_lookup_table <- data.frame(aging_technique = as.integer(c(0, 1, 2, 3, 4, 5)), 
+                                           aging_technique_name = c("No scale", "Scale & aged", "Scale - Poor", "Scale - Regen.", 
+                                                 "Scale - Unsure", "Defaulted"))
+
+#aging_technique_lookup_table <- as.table(c())
+### make sure the types of data match
+final_df <- final_df %>%
+  mutate(
+    preservative_code = as.character(preservative_code),
+    lake_code = as.character(lake_code)
+  )
+
+fish_species_code_lookup_table <- fish_species_code_lookup_table %>%
+  mutate(species_code = as.integer(species_code))
+
+preservative_code_lookup_table <- preservative_code_lookup_table %>%
+  mutate(preservative_code = as.character(preservative_code))
+
+preservative_code_weight_conversion_lookup_table <- preservative_code_weight_conversion_lookup_table %>%
+  mutate(preservative_code = as.character(preservative_code))
+
+lake_name_lookup_table <- lake_name_lookup_table %>%
+  mutate(lake_code = as.character(lake_code))
+
+### join the tables
+final_df <- final_df %>%
+  left_join(fish_species_code_lookup_table, by = "species_code") %>%
+  left_join(preservative_code_lookup_table, by = "preservative_code") %>%
+  left_join(preservative_code_weight_conversion_lookup_table, by = "preservative_code") %>%
+  left_join(lake_name_lookup_table, by = "lake_code") %>%
+  left_join(aging_technique_lookup_table, by = "aging_technique")
+
+# Reorganize columns order
+final_df <- final_df %>% 
+  relocate(process_date, processor, 
+           lake_code, lake_name = lake, trawl_date,
+           start_time, end_time, duration_mi, depth_m,  
+           trawl_number, fish_total, fish_id, species_code, fish_description,
+           fish_length_mm, fish_weight_g, standardized_weight_g, weight_conversion_formula, 
+           preservative_code, preservative_description, sample_number, 
+           scale, scale_book, comment, 
+           age, aging_technique, aging_technique_name)
+
+### add a source file 
+final_df <- final_df %>%
+  mutate(source_file = f)
+
+### adding a column and populating with my info 
+final_df <- final_df %>%
+  mutate(program_notes = "AA - Living Data Program - 2025")
+
+### save table in csv 
+write.csv(final_df, paste0(intermediate_out_folder,"/trawl", metadata_yy, "_SAS.csv"), row.names = FALSE)
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ TRAWL_BIOSAMPLE/01_code/summary_table.R
@@ -0,0 +1,135 @@
+###############################################################################
+##############                  TRAWL data                  ###################
+##############     summarizing SAS and .dat trawl files     ###################
+############## Authors: Alice Assmar (McGill Uni.), David   ###################
+############## Hunt (McGill Uni.),  Yuliya Shtymburski      ###################
+############## (U. Regina), Howard Stiff (DFO Nanaimo),     ###################
+##############        Athena Ogden (DFO Nanaimo)            ###################
+###############################################################################
+
+# getwd()
+# setwd("./LDP_ATS_Rescue")
+
+# Install necessary packages if they are not yet installed
+packages <- c("beepr", "dplyr", "lubridate","progress",
+              "purrr","stringr", "tibble", "tictoc", "tidyverse", "tools", "Rcpp", "haven")
+install.packages(setdiff(packages, row.names(installed.packages())))
+
+# Load necessary packages
+{
+  library(beepr)
+  library(dplyr)
+  library(lubridate)
+  library(purrr)
+  library(stringr)
+  library(tidyverse)
+  library(tools)
+  library(Rcpp) 
+  library(haven) # read SAS files
+}
+
+###############
+#### Read raw tables if necessary
+#trawlyy_sas <- read_sas("./TRAWL_BIOSAMPLE/00_raw_data/02_SAS_Data/trawl87.sas7bdat", NULL)
+#metadata_trawl_yy <- read_sas("./TRAWL_BIOSAMPLE/00_raw_data/02_SAS_Data/trlinf87.sas7bdat", NULL)
+
+#### Read csv tables
+intermediate_out_folder <- "./TRAWL_BIOSAMPLE/02_intermediate_out"
+trawl_file <- "trawl88"
+# Some files has upper case names that I must correct, for now, use this when necessary
+#trawl_file_dat <- "TRAWL96"
+
+final_csv_dat <- read.csv(paste0(intermediate_out_folder,"/", trawl_file, "_DAT.csv"))
+final_csv_sas <- read.csv(paste0(intermediate_out_folder,"/", trawl_file, "_SAS.csv"))
+
+#### Summarize dat and SAS tables
+summary(final_csv_dat)
+summary(final_csv_sas)
+# How many date the lake was surveyed
+aggregate(trawl_date ~ lake_code, data = final_csv_dat, FUN = function(x) length(unique(x)))
+aggregate(trawl_date ~ lake_code, data = final_csv_sas, FUN = function(x) length(unique(x)))
+
+# How many date the lake was surveyed
+aggregate(cbind(trawl_date, trawl_number) ~ lake_code, data = final_csv_dat, FUN = function(x) length(unique(x)))
+aggregate(cbind(trawl_date, trawl_number) ~ lake_code, data = final_csv_sas, FUN = function(x) length(unique(x)))
+
+# Check how many column match
+# create a data set with the info I want to check
+df_dat <- tibble(unique = unique(paste(final_csv_dat$fish_unique_ID, final_csv_dat$lake_code, final_csv_dat$trawl_number, final_csv_dat$trawl_date, final_csv_dat$fish_id))) 
+df_sas <- tibble(unique = unique(paste(final_csv_sas$fish_unique_ID, final_csv_sas$lake_code, final_csv_sas$trawl_number, final_csv_sas$trawl_date, final_csv_sas$fish_id)))
+
+df_dat <- df_dat %>%
+  separate(col = unique, into = c("fish_unique_ID","lake_code", "trawl_number","trawl_date", "fish_id"), sep = " ")
+
+df_sas <- df_sas %>%
+  separate(col = unique, into = c("fish_unique_ID", "lake_code", "trawl_number", "trawl_date", "fish_id"), sep = " ")
+
+# line up identical rows
+columns <- c("lake_code", "trawl_number", "trawl_date", "fish_id")
+
+# Check for leading/trailing whitespace
+for (col in columns) {
+  df_sas[[col]] <- trimws(df_sas[[col]])
+}
+
+for (col in columns) {
+  df_dat[[col]] <- trimws(df_dat[[col]])
+}
+
+# Merge the database using the unique ID to combine
+merged_df <- merge(df_sas [, c("fish_unique_ID", columns), drop = FALSE],
+                   df_dat [, c("fish_unique_ID", columns), drop = FALSE],
+                   by = "fish_unique_ID",
+                   all = TRUE, # Keep all
+                   suffixes = c(".sas", ".dat"))
+
+# Compare the columns
+# store the names of the columns
+cols_sas <- paste0(columns,".sas")
+cols_dat <- paste0(columns,".dat")
+
+# Check if they have the same class
+sapply(merged_df[cols_sas], class)
+sapply(merged_df[cols_dat], class)
+
+
+# Create a match/not match column
+merged_df <- merged_df %>%
+  rowwise() %>%
+  mutate(
+    match = {
+      left  <- c_across(paste0(cols_sas))
+      right <- c_across(paste0(cols_dat))
+      
+      # compare the rows
+      compare_rows <- mapply(function(x, y) {
+        if (is.na(x) & is.na(y)) TRUE
+        else if (is.na(x) | is.na(y)) FALSE
+        else x ==y
+      }, left, right)
+      # Check if all match, label as "MATCH" or " NOT MATCH"
+      if (all(compare_rows)) "MATCH" else "NOT MATCH"
+    }
+  ) %>%
+  ungroup()
+
+# Save final table in csv
+write.csv(merged_df, paste0(intermediate_out_folder, "/", trawl_file, "_inventory.csv"), row.names = FALSE)
+
+# Count number of matchs
+sum(merged_df$match == "MATCH", na.rm = TRUE)
+sum(merged_df$match == "NOT MATCH", na.rm = TRUE)
+
+# Check length for unique lakes and which lakes are present
+length(unique(final_csv_dat$lake_code))
+unique(final_csv_dat$lake_code)
+
+length(unique(final_csv_sas$lake_code))
+unique(final_csv_sas$lake_code)
+
+unique(paste(final_csv_dat$lake_code, final_csv_dat$trawl_date))
+unique(paste(final_csv_sas$lake_code, final_csv_sas$trawl_date))
+
+# Read inventory
+final_csv_inventory <- read.csv(paste0(intermediate_out_folder,"/", trawl_file, "_inventory.csv"))
+
